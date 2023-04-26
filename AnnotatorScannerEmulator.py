64dbdbc48bf319da253b9e784a4ff6ce91de3356
Raghu Ganapathy <rgana001@ucr.edu>
2023-04-26 10:24:55 -0700

    adds the changes from catchup call 04

--- AnnotatorScannerEmulator.py
+++ AnnotatorScannerEmulator.py
@@ -42,14 +42,23 @@ class AnnotatorScannerEmulator:
         # If we got here, the file was downloaded successfully
         return True
 
+    # Function to build a tsv file, given an array of paths
+    def buildTsvFile(self, pathToNullAway, pathToAnnotator):
+         # Open the file for writing
+        with open("config/paths.tsv", 'w') as f:
+            # Write the data rows
+            f.write(f"{pathToNullAway}\t{pathToAnnotator}\n")
     def runAnnotator(self):
-        buildCommand = "java -jar " + self.pathToJar + " -d /Users/raghuganapathy/Desktop/Masters Project/Python Scripts/out " + " -i com.example.Initializer " + " --build-command " + "cd {} && ./gradlew build -x test".format(self.pathToTargetProject)
+        buildCommand = "java -jar " + self.pathToJar + " -d \"/Users/raghuganapathy/Desktop/temp/PythonScripts/out\" " + "-cp \"/Users/raghuganapathy/Desktop/temp/PythonScripts/config/paths.tsv\""  " -i com.example.Initializer " + " --build-command " + "\"cd {} && ./gradlew build -x test\"".format(self.pathToTargetProject) \
+        
         print("\n")
         print(buildCommand)
     
 if __name__ == "__main__":
 
     pathToTargetProject = "/Users/raghuganapathy/Desktop/JavaProjects/exttemp"
+    pathToNullAway = "/Users/raghuganapathy/Desktop/temp/PythonScripts/config/nullaway.xml"
+    pathToAnnotator = "/Users/raghuganapathy/Desktop/temp/PythonScripts/config/scanner.xml"
 
     a = AnnotatorScannerEmulator(pathToTargetProject=pathToTargetProject)
     # Jar Metadata
@@ -57,6 +66,6 @@ if __name__ == "__main__":
     jarName = "annotator-core-1.3.6.jar"
     # Download the jar file and store it in the temp folder
     a.getJarFileFromNet(jarUrl, jarName)
-
+    a.buildTsvFile(pathToNullAway, pathToAnnotator)
     # Run the annotator
     a.runAnnotator()
